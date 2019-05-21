02c747dd7c0fe4eba5576628d0a84d23b29c6749
Backporter <29647778+Backporter@users.noreply.github.com>
2019-05-21 18:22:10 -0400

    Main code
    The source code

---
+++ PS3-Skyrim-Animation-Converter/Credits.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Net;
+using System.Threading;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+namespace PS3_Skyrim_Animation_Converter
+{
+    public partial class Credits : Form
+    {
+        public Credits()
+        {
+            InitializeComponent();
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process.Start("https://discord.gg/twpSyYp");
+        }
+    }
+}
